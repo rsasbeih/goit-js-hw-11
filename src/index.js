a37079ea306448e0c04012ef70e83e295748bcec
Ruba Sbeih <rsasbeih@gmail.com>
2022-09-21 21:17:18 +0300

    after review edits

--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@ const loadMore = document.querySelector(".load-more");
 // Described in documentation
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-
 let lightbox = new SimpleLightbox('.gallery a', {});
 let page = 1;
 let totalHits = 0;
+let totalHitsValue = 0;
 form.addEventListener('submit', onSubmit);
 loadMore.addEventListener('click', loadMoreImages);
 
@@ -21,25 +21,26 @@ function onSubmit(event) {
         hits.innerHTML = '';
         page = 1;
         getHits(input.value.trim()).then(() => {
-                loadMore.style.display = "block";
-                Notiflix.Notify.success(`Hooray! We found ${response.data.totalHits} images.`);
+                if (totalHitsValue > 0) {
+                        Notiflix.Notify.success(`Hooray! We found ${totalHitsValue} images.`);
+                        loadMore.style.display = "block";
+                }
         });
 }
 
 async function getHits(input) {
         const response = await axios.get(`https://pixabay.com/api/?key=30033102-2311f4d2ef3dab127d4c7748f&q=${input}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`);
-        if (response.data.hits.length == 0) {
+        totalHitsValue = response.data.totalHits;
+        if (response.data.total == 0) {
                 Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
                 return;
         }
-        lightbox.refresh();
-        totalHits += response.data.hits;
+        totalHits += response.data.hits.length;
         if (response.data.totalHits == totalHits) {
                 Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
                 loadMore.style.display = "none";
                 return;
         }
-        console.log(Object.values(response));
         let results = response.data.hits;
         let markup = results.map(({
                 webformatURL,
@@ -73,18 +74,29 @@ async function getHits(input) {
               </div>
           </a>`);
         hits.insertAdjacentHTML('beforeend', markup);
+        lightbox.refresh();
+        return response;
 }
 function loadMoreImages(event) {
         event.preventDefault();
+        lightbox.refresh();
         page += 1;
         getHits(input.value.trim()).then(() => {
-                const { height: cardHeight } = document
-                        .querySelector(".gallery")
-                        .firstElementChild.getBoundingClientRect();
+                if (totalHits <= totalHitsValue) {
+                        const { height: cardHeight } = document
+                                .querySelector(".gallery")
+                                .firstElementChild.getBoundingClientRect();
 
-                window.scrollBy({
-                        top: cardHeight * 2,
-                        behavior: "smooth",
-                });
+                        window.scrollBy({
+                                top: cardHeight * 2,
+                                behavior: "smooth",
+                        });
+
+                } else {
+                        console.log(totalHits);
+                        Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+                        loadMore.style.display = "none";
+                }
+
         });
 }
